3c22bef645d0ccaaf526bce2ffbdbf1a6c38d94e
ferrianmfatichin97 <ferrianmfatichin97@gmail.com>
2025-11-14 16:04:07 +0700

    Refactor DatabaseSeeder to create roles and users with assigned roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission; // Tidak digunakan di sini, tapi bisa berguna nanti
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,11 +18,46 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $adminRole = Role::firstOrCreate(
+            ['name' => 'admin'],
+            ['guard_name' => 'web']
+        );
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $akuntingRole = Role::firstOrCreate(
+            ['name' => 'akunting'],
+            ['guard_name' => 'web']
+        );
+
+        $csRole = Role::firstOrCreate(
+            ['name' => 'cs'],
+            ['guard_name' => 'web']
+        );
+
+        $adminUser = User::create([
+            'name' => 'Admin Super',
+            'email' => 'admin@bankdptaspen.co.id',
+            'password' => Hash::make('password'),
+            'email_verified_at' => now(),
         ]);
+
+        $adminUser->assignRole($adminRole);
+
+        $akuntingUser = User::create([
+            'name' => 'Muhammad Yassin',
+            'email' => 'm.yassin@bankdptaspen.co.id',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        $akuntingUser->assignRole($akuntingRole);
+
+        $csUser = User::create([
+            'name' => 'Customer Service',
+            'email' => 'cs@bankdptaspen.co.id',
+            'password' => Hash::make('12345678'),
+            'email_verified_at' => now(),
+        ]);
+
+        $csUser->assignRole($csRole);
+
     }
 }
